7edbaf3c473cfaf9da08ad0cd23e4a8d27f4051e
John Royal <34844819+john-royal@users.noreply.github.com>
2025-03-26 23:42:07 -0400

    style: import server functions from correct file

--- src/routes/auth.index.tsx
+++ src/routes/auth.index.tsx
@@ -1,16 +1,6 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-import { createServerFn, useServerFn } from "@tanstack/react-start";
-import { fetchAuth, getAppSession } from "~/utils/auth";
-
-export const redirectToGitHub = createServerFn().handler(async () => {
-  const json = await fetchAuth({ action: "authorize" });
-  const session = await getAppSession();
-  await session.update({
-    type: "challenge",
-    state: json.state,
-  });
-  throw redirect({ href: json.url });
-});
+import { createFileRoute } from "@tanstack/react-router";
+import { useServerFn } from "@tanstack/react-start";
+import { redirectToGitHub } from "~/utils/actions";
 
 export const Route = createFileRoute("/auth/")({
   component: RouteComponent,
